4e12a58e6e6f51baf59b7eb5f6adffa743ceffe5
TiffianyYLZ <649270813@qq.com>
2019-03-28 17:45:56 +0800

    产品信息列表

---
+++ p2p-common/src/main/java/com/bjpowernode/p2p/common/constant/Constants.java
@@ -0,0 +1,41 @@
+package com.bjpowernode.p2p.common.constant;
+
+/**
+ * ClassNameConstants
+ * Package_com.bjpowernode.p2p.constant
+ * Description:
+ *
+ * @Date:2019/3/26 13:29
+ * @Author:YangLei
+ */
+public class Constants {
+    /**
+     * 历史平均年化收益率
+     */
+    public static final String HISTORY_AVERAGE_RATE ="historyAverageRate";
+
+    /**
+     * 平台注册总人数
+     */
+    public static final String ALL_USER_COUNT = "allUserCount";
+
+    /**
+     * 累计投资金额
+     */
+    public static final String ALL_BID_MONEY = "allBidMoney";
+
+    /**
+     * 新手宝
+     */
+    public static final Integer PRODUCT_TYPE_X = 0;
+
+    /**
+     * 优选
+     */
+    public static final Integer PRODUCT_TYPE_U = 1;
+
+    /**
+     * 散标
+     */
+    public static final Integer PRODUCT_TYPE_S = 2;
+}
